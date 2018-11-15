fdd0874a18e6998a4ee209e0671bc16b0ca0e409
aubreeabril <aubree.abril@gmail.com>
2018-11-15 15:23:49 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,14 @@
 # Implement your procedural solution here!
+
+def largest_palindrome_product
+  highest = 0
+  (100...999).each do |num1|
+    (100...999).each do |num2|
+      prod = num1 * num2
+      if (prod.to_s == prod.to_s.reverse && prod > highest)
+        highest = prod
+      end
+    end
+  end
+  highest
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,23 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  attr_accessor :answer
+
+def initialize
+  @answer = pal_product
+end
+
+def pal_product
+  highest = 0
+
+  (100...999).each do |num1|
+    (100...999).each do |num2|
+      product = num1 * num2
+      if product.to_s == product.to_s.reverse && product > highest
+        highest = product
+      end
+    end
+  end
+  highest
+end
+
+end
